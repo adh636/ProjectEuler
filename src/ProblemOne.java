b08bedb08a721ac3f5d4a5d7181cc4a33d3c9925
Adam <adh636@gmail.com>
2016-06-26 22:58:01 -0400

    update solution to problem one

--- src/ProblemOne.java
+++ src/ProblemOne.java
@@ -1,11 +1,11 @@
 /*
- * Multiples of 3 and 5
-	Problem 1
-	If we list all the natural numbers below 10 that are multiples of 3 or 5, 
-	we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Multiples of 3 and 5
+Problem 1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-	Find the sum of all the multiples of 3 or 5 below 1000.
- */
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
 
 public class ProblemOne {
 
@@ -13,9 +13,9 @@ public class ProblemOne {
 		System.out.println(sumOfMultiples(1000));
 	}
 	
-	static int sumOfMultiples(int num) {
+	static int sumOfMultiples(int n) {
 		int sum = 0;
-		for (int i = 0; i < num; i++) {
+		for (int i = 0; i < n; i++) {
 			if (i % 3 == 0 || i % 5 == 0) {
 				sum += i;
 			}
